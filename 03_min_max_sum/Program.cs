d71157f00bf2d2b9038da5260846e5cdc402cc38
olexandr.finyuk <olexandr.finyuk@gmail.com>
2018-09-18 20:47:55 +0300

    Add project files.

---
+++ 03_min_max_sum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_min_max_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
